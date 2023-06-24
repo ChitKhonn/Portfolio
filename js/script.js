e4afa7a9a020134fa49bcb20fce564562c865d78
ckc <chitkhonncho9@gmail.com>
2023-06-24 21:49:20 +0630

    finish

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================= typing animation  ========================= */
+var typed = new Typed(".typing",{
+    strings:["","Full-Stack Developer","Software Developer","Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
